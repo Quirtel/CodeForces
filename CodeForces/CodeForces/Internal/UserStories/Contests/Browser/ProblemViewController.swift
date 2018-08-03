22d026f4d0b2c349de77e96a7c2f56868a152ca1
Andrey Mityulin <sk.quirtel@gmail.com>
2018-08-03 12:23:56 +0700

    Fix browser to run asynchronously

--- CodeForces/CodeForces/Internal/UserStories/Contests/Browser/ProblemViewController.swift
+++ CodeForces/CodeForces/Internal/UserStories/Contests/Browser/ProblemViewController.swift
@@ -6,7 +6,7 @@ import WebKit
 class ProblemViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView = WKWebView()
-    
+    var backgroundTasks = DispatchQueue(label: "background")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,34 +21,45 @@ class ProblemViewController: UIViewController, WKNavigationDelegate {
     }
     
     func configure(with model: TaskCellModel) {
-        
-        let url = URL(string: "https://www.codeforces.com/contest/\(model.contestId)/problem/\(model.index)?mobile=true")!
-        
-        let request = URLRequest(url: url)
-        
-        do {
-            let html = try String(contentsOf: url, encoding: .utf8)
+        backgroundTasks.async {
+            let url = URL(string: "https://www.codeforces.com/contest/\(model.contestId)/problem/\(model.index)?mobile=true")!
             
-            let doc: Document = try SwiftSoup.parse(html)
+            let request = URLRequest(url: url)
             
-            try doc.getElementsByClass("second-level-menu").first()?.remove()
-            try doc.getElementsByClass("roundbox menu-box").first()?.remove()
-            try doc.getElementsByClass("lang-chooser").first()?.remove()
-            try doc.getElementsByClass("menu-box").first()?.remove()
-            try doc.getElementsByClass("mobile-toolbar").first()?.remove()
-            try doc.getElementById("swipe-sidebar")?.remove()
-            try doc.head()?.append("""
+            do {
+                let html = try String(contentsOf: url, encoding: .utf8)
+                
+                let doc: Document = try SwiftSoup.parse(html)
+                
+                try doc.getElementsByClass("second-level-menu").first()?.remove()
+                try doc.getElementsByClass("roundbox menu-box").first()?.remove()
+                try doc.getElementsByClass("lang-chooser").first()?.remove()
+                try doc.getElementsByClass("menu-box").first()?.remove()
+                try doc.getElementsByClass("mobile-toolbar").first()?.remove()
+                try doc.getElementById("swipe-sidebar")?.remove()
+                try doc.head()?.append("""
                 <style>div.ttypography p {font-size: 1.7em}
                 .problem-statement .sample-tests pre {font-size: 1.7em}
                 .problem-statement .sample-tests .title {font-size: 1.7em}
+                .problem-statement .time-limit, .problem-statement .memory-limit,
+                .problem-statement .input-file, .problem-statement .output-file {font-size: 1.13em}
+                .problem-statement .header .title {font-size: 1.9em}
                 </style>
                 """
-            )
-            
-            webView.loadHTMLString(try doc.html(), baseURL: url)
-        }
-        catch (let error) {
-            print(error)
+                )
+                
+                DispatchQueue.main.async {
+                    do {
+                        self.webView.loadHTMLString(try doc.html(), baseURL: url)
+                    }
+                    catch (let error) {
+                        print(error)
+                    }
+                }
+            }
+            catch (let error) {
+                print(error)
+            }
         }
     }
 }
